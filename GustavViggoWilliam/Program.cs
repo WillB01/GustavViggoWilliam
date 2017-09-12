7366fb81e546463e52ff1a1de94b8cb61b5458b3
WillB01 <wb.william@outlook.com>
2017-09-12 10:41:34 +0200

    ändrat

--- GustavViggoWilliam/Program.cs
+++ GustavViggoWilliam/Program.cs
@@ -6,7 +6,7 @@ namespace GustavViggoWilliam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hej på er!");
+            Console.WriteLine("Hej på er!!!!!");
         }
     }
 }
